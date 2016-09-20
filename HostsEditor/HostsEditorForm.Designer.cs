90c768a1f0e491f3d4d609a48e489f281a3abcae
Bananowars <bananowars@gmail.com>
2016-09-20 21:32:40 +0300

    Final

---
+++ HostsEditor/HostsEditorForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace HostsEditor
+{
+    partial class HostsEditorForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HostsEditorForm));
+            this.urlString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lockUrl = new System.Windows.Forms.Button();
+            this.unlockUrl = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.поУмолчаниюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // urlString
+            // 
+            this.urlString.Location = new System.Drawing.Point(45, 360);
+            this.urlString.Name = "urlString";
+            this.urlString.Size = new System.Drawing.Size(168, 20);
+            this.urlString.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "http://";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 345);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Введите адресс веб-сайта:";
+            // 
+            // lockUrl
+            // 
+            this.lockUrl.Location = new System.Drawing.Point(45, 386);
+            this.lockUrl.Name = "lockUrl";
+            this.lockUrl.Size = new System.Drawing.Size(168, 23);
+            this.lockUrl.TabIndex = 1;
+            this.lockUrl.Text = "Заблокировать";
+            this.lockUrl.UseVisualStyleBackColor = true;
+            this.lockUrl.Click += new System.EventHandler(this.addUrl_Click);
+            // 
+            // unlockUrl
+            // 
+            this.unlockUrl.Location = new System.Drawing.Point(45, 310);
+            this.unlockUrl.Name = "unlockUrl";
+            this.unlockUrl.Size = new System.Drawing.Size(168, 23);
+            this.unlockUrl.TabIndex = 2;
+            this.unlockUrl.Text = "Разблокировать";
+            this.unlockUrl.UseVisualStyleBackColor = true;
+            this.unlockUrl.Visible = false;
+            this.unlockUrl.Click += new System.EventHandler(this.removeUrl_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(264, 24);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.поУмолчаниюToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // поУмолчаниюToolStripMenuItem
+            // 
+            this.поУмолчаниюToolStripMenuItem.Name = "поУмолчаниюToolStripMenuItem";
+            this.поУмолчаниюToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.поУмолчаниюToolStripMenuItem.Text = "По умолчанию";
+            this.поУмолчаниюToolStripMenuItem.Click += new System.EventHandler(this.поУмолчаниюToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 277);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HostsEditor.Properties.Resources.vkontakte;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 415);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 35);
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // HostsEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(264, 458);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.urlString);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lockUrl);
+            this.Controls.Add(this.unlockUrl);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HostsEditorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HostsEditor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button lockUrl;
+        private System.Windows.Forms.Button unlockUrl;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem поУмолчаниюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HostsEditor/HostsEditorForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace HostsEditor
+{
+    public partial class HostsEditorForm : Form
+    {
+        public HostsEditorForm()
+        {
+            InitializeComponent();
+            ReadFile();
+        }
+        Regex myReg = new Regex(@"\.(ru|com|ua|su|net|me|info)\b");
+        string path = @"C:\Windows\System32\drivers\etc\hosts";
+        async private void removeUrl_Click(object sender, EventArgs e)
+        {
+            string strUrl;
+            if (listBox1.SelectedItem != null)
+            {
+                strUrl = listBox1.SelectedItem.ToString();
+                listBox1.Items.Remove(listBox1.SelectedItem);
+
+                var myList = new ArrayList();
+                File.SetAttributes(path, FileAttributes.Normal);
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string str;
+                    var myReg1 = new Regex(strUrl);
+                    while (true)
+                    {
+                        str = await sr.ReadLineAsync();
+                        if (str == null) { break; }
+
+                        if (myReg1.IsMatch(str) == false)
+                        {
+                            myList.Add(str);
+                        }
+                    }
+                    sr.Close();
+                }
+                using (StreamWriter sw = new StreamWriter(path, false))
+                {
+                    foreach (var item in myList)
+                    {
+                        sw.WriteLine(item);
+                    }
+                    sw.Close();
+                }
+                ReadFile();
+                File.SetAttributes(path, FileAttributes.ReadOnly);
+            }
+            if (listBox1.SelectedItem == null)
+                unlockUrl.Visible = false;
+        }
+        async private void addUrl_Click(object sender, EventArgs e)
+        {
+            if (urlString.Text != null && myReg.IsMatch(urlString.Text) == true)
+            {
+                File.SetAttributes(path, FileAttributes.Normal);
+                StreamWriter sw = new StreamWriter(path, true);
+                await sw.WriteLineAsync("127.0.0.1 " + urlString.Text);
+                sw.Close();
+                File.SetAttributes(path, FileAttributes.ReadOnly);
+                ReadFile();
+                if (listBox1.SelectedItem == null)
+                    unlockUrl.Visible = false;
+                urlString.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Введите правельный адресс", "Ошибка");
+            }
+        }
+        async private void ReadFile()
+        {
+            listBox1.Items.Clear();
+            StreamReader sr = new StreamReader(path);
+            string str = await sr.ReadToEndAsync();
+            foreach (Match m in Regex.Matches(str, @"127.0.0.1\s+.+\.(ru|com|ua|su|net|me|info)\b"))
+                listBox1.Items.Add(Regex.Match(m.Value, @"\b\S+\.(ru|com|ua|su|net|me|info)\b"));
+            sr.Close();
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            unlockUrl.Visible = true;
+        }
+        private void поУмолчаниюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string str1 = "# Copyright (c) 1993-2009 Microsoft Corp.\n#\n# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.\n#\n# This file contains the mappings of IP addresses to host names. Each\n# entry should be kept on an individual line. The IP address should\n# be placed in the first column followed by the corresponding host name.\n# The IP address and the host name should be separated by at least one\n# space.\n#\n# Additionally, comments (such as these) may be inserted on individual\n# lines or following the machine name denoted by a '#' symbol.\n#\n# For example:\n#\n#      102.54.94.97     rhino.acme.com          # source server\n#       38.25.63.10     x.acme.com              # x client host\n\n# localhost name resolution is handled within DNS itself.\n#	127.0.0.1       localhost\n#	::1             localhost\n\n";
+            File.SetAttributes(path, FileAttributes.Normal);
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                sw.Write(str1);
+                sw.Close();
+            }
+            ReadFile();
+            File.SetAttributes(path, FileAttributes.ReadOnly);
+        }
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            urlString.Text = "vk.com";
+        }
+    }
+}
